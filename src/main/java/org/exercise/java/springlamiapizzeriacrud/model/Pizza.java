fe1c636fdccddf572c4097a4463b0a2443291489
MirkoGLostia <giggi.torre@mail.com>
2023-11-09 15:05:54 +0100

    creation entity pizza

--- src/main/java/org/exercise/java/springlamiapizzeriacrud/model/Pizza.java
+++ src/main/java/org/exercise/java/springlamiapizzeriacrud/model/Pizza.java
@@ -2,6 +2,8 @@ package org.exercise.java.springlamiapizzeriacrud.model;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table (name = "pizzas")
 public class Pizza {
@@ -9,4 +11,50 @@ public class Pizza {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    private String name;
+    @Lob
+    private String description;
+    private String image;
+    private BigDecimal price;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
